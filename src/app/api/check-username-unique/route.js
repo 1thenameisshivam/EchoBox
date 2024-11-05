0418b1988882902d692a22ce7496790e1640a0a2
1thenameisshivam <1thenameisshivam@gmail.com>
2024-11-05 17:23:27 +0530

    check-username-uniqur api created

---
+++ src/app/api/check-username-unique/route.js
@@ -0,0 +1,49 @@
+import { z } from "zod";
+import dbConnect from "@/config/dbconnect";
+import User from "@/models/user";
+import { NextResponse } from "next/server";
+import { userNameSchema } from "@/validation/signupSchema";
+
+const checkUsernameUniqueSchema = z.object({
+  username: userNameSchema,
+});
+
+export const GET = async (req) => {
+  try {
+    await dbConnect();
+
+    const { searchParams } = new URL(req.url);
+    const queryParam = {
+      username: searchParams.get("username"),
+    };
+    // Validate the query parameters
+    const result = checkUsernameUniqueSchema.safeParse(queryParam);
+    if (!result.success) {
+      return NextResponse.json(
+        {
+          message: result.error.format().username?._errors || [],
+          success: false,
+        },
+        { status: 400 }
+      );
+    }
+    const { username } = result.data;
+    const exestingUser = await User.findOne({ username, isVerified: true });
+    if (exestingUser) {
+      return NextResponse.json(
+        { message: "Username already taken", success: false },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json({
+      message: "Username is available",
+      success: true,
+    });
+  } catch (error) {
+    console.log("error in checking username", error);
+    return NextResponse.json(
+      { message: "error in checking username", success: false },
+      { status: 500 }
+    );
+  }
+};
