d1f2245712d67165e109a97efc1fc2df66411f7b
1thenameisshivam <1thenameisshivam@gmail.com>
2024-11-05 13:02:19 +0530

    resend email configured

---
+++ src/app/emails/VerificationEmail.js
@@ -0,0 +1,144 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import React from "react";
+
+const VerificationEmail = ({ username = "User", otp = "123456" }) => {
+  return (
+    <Html>
+      <Head />
+      <Preview>EchoBox Email Verification</Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Section style={coverSection}>
+            <Section style={upperSection}>
+              <Heading style={h1}>Verify your email address</Heading>
+              <Text style={mainText}>
+                Hi {username},<br />
+                Thanks for joining EchoBox! To complete your sign-up, please
+                enter the verification code below. If you didn&apos;t request
+                this, feel free to ignore this email.
+              </Text>
+              <Section style={verificationSection}>
+                <Text style={verifyText}>Verification code</Text>
+                <Text style={codeText}>{otp}</Text>
+                <Text style={validityText}>
+                  (This code is valid for 10 minutes)
+                </Text>
+              </Section>
+            </Section>
+            <Hr />
+            <Section style={lowerSection}>
+              <Text style={cautionText}>
+                EchoBox will never ask you to disclose your password or payment
+                details via email.
+              </Text>
+            </Section>
+          </Section>
+          <Text style={footerText}>
+            This message was sent by EchoBox. © 2024 EchoBox, Inc. All rights
+            reserved. View our{" "}
+            <Link
+              href="https://echobox.com/privacy-policy"
+              target="_blank"
+              style={link}
+            >
+              privacy policy
+            </Link>
+            .
+          </Text>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+const main = {
+  backgroundColor: "#fff",
+  color: "#212121",
+};
+
+const container = {
+  padding: "20px",
+  margin: "0 auto",
+  backgroundColor: "#eee",
+};
+
+const h1 = {
+  color: "#333",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: "20px",
+  fontWeight: "bold",
+  marginBottom: "15px",
+};
+
+const link = {
+  color: "#2754C5",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: "14px",
+  textDecoration: "underline",
+};
+
+const text = {
+  color: "#333",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: "14px",
+  margin: "24px 0",
+};
+
+const coverSection = { backgroundColor: "#fff" };
+
+const upperSection = { padding: "25px 35px" };
+
+const lowerSection = { padding: "25px 35px" };
+
+const footerText = {
+  ...text,
+  fontSize: "12px",
+  padding: "0 20px",
+};
+
+const verifyText = {
+  ...text,
+  margin: 0,
+  fontWeight: "bold",
+  textAlign: "center",
+};
+
+const codeText = {
+  ...text,
+  fontWeight: "bold",
+  fontSize: "36px",
+  margin: "10px 0",
+  textAlign: "center",
+};
+
+const validityText = {
+  ...text,
+  margin: "0px",
+  textAlign: "center",
+};
+
+const verificationSection = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const mainText = { ...text, marginBottom: "14px" };
+
+const cautionText = { ...text, margin: "0px" };
+
+export default VerificationEmail;
---
+++ src/app/helpers/sendVerificationEmail.js
@@ -0,0 +1,19 @@
+import VerificationEmail from "../emails/VerificationEmail";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API);
+
+export const sendVerificationEmail = async (email, verifyCode, username) => {
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "echobox || verification code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+    return { sucess: true, message: "Verification email sent" };
+  } catch (error) {
+    console.log(error.message);
+    return { sucess: false, message: "Error sending verification email" };
+  }
+};
