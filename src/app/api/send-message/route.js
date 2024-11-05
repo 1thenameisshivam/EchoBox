6720a0750dd7ba60657754e7faea9db3c06931a6
1thenameisshivam <1thenameisshivam@gmail.com>
2024-11-05 22:48:24 +0530

    send-messages api created

---
+++ src/app/api/send-message/route.js
@@ -0,0 +1,34 @@
+import dbConnect from "@/config/dbconnect";
+import User from "@/models/user";
+
+export const POST = async (req) => {
+  try {
+    await dbConnect();
+    const { username, content } = await req.json();
+    const user = await User.findOne({ username });
+    if (!user) {
+      return NextResponse.json(
+        { message: "User not found", success: false },
+        { status: 404 }
+      );
+    }
+    if (!user.isAcceptingMessages) {
+      return NextResponse.json(
+        { message: "User is not accepting messages", success: false },
+        { status: 400 }
+      );
+    }
+    user.messages.push({ content, createdAt: new Date() });
+    await user.save();
+    return NextResponse.json({
+      message: "Message sent successfully",
+      success: true,
+    });
+  } catch (err) {
+    console.log("error from sendMessage ", err);
+    return NextResponse.json(
+      { message: "error in sending messages", success: false },
+      { status: 500 }
+    );
+  }
+};
