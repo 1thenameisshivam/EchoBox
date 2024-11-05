9afd6d36674ab4fceec57e222124a80a0ffcb182
1thenameisshivam <1thenameisshivam@gmail.com>
2024-11-05 20:10:42 +0530

    verify-code api created

---
+++ src/app/api/verify-code/route.js
@@ -0,0 +1,48 @@
+import User from "@/models/user";
+import dbConnect from "@/config/dbconnect";
+import { verifySchema } from "@/validation/verifySchema";
+import { NextResponse } from "next/server";
+export const POST = async (req) => {
+  try {
+    await dbConnect();
+    const { code, username } = await req.json();
+    const result = verifySchema.safeParse({ code });
+    if (!result.success) {
+      return NextResponse.json(
+        { message: result.error.format().code?._errors || [], success: false },
+        { status: 400 }
+      );
+    }
+    const user = await User.findOne({ username });
+    if (!user) {
+      return NextResponse.json(
+        { message: "User not found", success: false },
+        { status: 404 }
+      );
+    }
+    const isCodeValid = user.verifyCode === code;
+    const isCodeExpired = new Date(user.verifyCodeExpires) > new Date();
+    if (!isCodeValid || !isCodeExpired) {
+      return NextResponse.json(
+        {
+          message: "Invalid code please signup again to get new code",
+          success: false,
+        },
+        { status: 400 }
+      );
+    }
+    user.isVerified = true;
+
+    await user.save();
+    return NextResponse.json(
+      { message: "User verified successfully", success: true },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { message: "error in velidating code", success: false },
+      { status: 500 }
+    );
+  }
+};
