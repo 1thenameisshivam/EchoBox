705e9916ccec265033107ddb866db8c2ae36f57d
1thenameisshivam <1thenameisshivam@gmail.com>
2024-11-05 11:03:07 +0530

    message and user schema created

---
+++ src/models/user.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "username is required"],
+    trim: true,
+    unique: [true, "username is already taken"],
+  },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+    trim: true,
+    lowercase: true,
+    unique: [true, "email is already registered"],
+    match: [/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/, "email is invalid"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "varifyCode is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verifyCodeExpires: {
+    type: Date,
+    required: [true, "varifyCodeExpires is required"],
+  },
+  isAcceptingMessages: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+  ],
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
